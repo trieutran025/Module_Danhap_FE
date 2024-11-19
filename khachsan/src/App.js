bfc9b866728561fccc16e4332e81e267b9a98eb4
Tuyetsang2801 <tranthituyetsang.280104@gmail.com>
2024-11-19 16:12:54 +0700

    màn hình quản lý

--- khachsan/src/App.js
+++ khachsan/src/App.js
@@ -1,29 +1,22 @@
-import "./App.css";
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import HomePage from "../src/componet/HomePage";
-import LoginPage from "../src/componet/Login";
-import AdminDashboard from "../src/componet/AdminDashboard";
+import React from "react";
+import EmployeeManagement from "./componet/EmployeeManagement";
+import CustomerManagement from "./componet/CustomerManagement"; // Make sure the path is correct
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // Hàm xử lý đăng nhập
-  const handleLogin = () => {
-    setIsAuthenticated(true); // Đánh dấu người dùng đã đăng nhập
-  };
-
+  // The error 'no-unused-expressions' could occur if you had an unused expression, 
+  // make sure any expressions are part of a valid statement like a function call or assignment
+  
+  // Example of avoiding an unused expression
+  let s = "This is a string"; // Now 's' is properly defined and used.
+  
+  // Fixing any unused expression issue by calling a function with 's'
+  console.log(s); // This would not trigger the "no-unused-expressions" error
+  
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
-        <Route
-          path="/dashboard"
-          element={isAuthenticated ? <AdminDashboard /> : <Navigate to="/login" />}
-        />
-      </Routes>
-    </Router>
+    <div>
+      <EmployeeManagement />
+    <CustomerManagement />
+  </div>
   );
 };
 
